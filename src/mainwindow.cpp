fc8b26eee5961335505055bb0c16f3f183f8ddb1
Joshua Hertz <44120268+joshuahertz@users.noreply.github.com>
2025-11-13 14:05:13 -0500

    Add files via upload

---
+++ src/mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    setWindowTitle("Qt qmake architecture example");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
